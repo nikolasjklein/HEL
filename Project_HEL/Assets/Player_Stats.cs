7f0f3f1a6a0ee96bd4f7c1bea8a36a96dafe33ae
nikolasjklein <the@nikolasjklein.com>
2019-05-02 17:05:31 +0200

    Nikolas - added InGame HUD with working Health and Armour Sliders

--- Project_HEL/Assets/Player_Stats.cs
+++ Project_HEL/Assets/Player_Stats.cs
@@ -11,17 +11,38 @@ public class Player_Stats : MonoBehaviour
     [Header("Player Health Text")]
     [Tooltip("The text which represents the Player Health on the GUI")]
     public Text playerHealth;
+    public Slider playerHealthBar;
+
+    public int pA;
+    public Text playerArmour;
+    public Slider playerArmourBar;
 
     public void Start()
     {
         //set player health to 100 at start
         pL = 100;
+        pA = 0;
     }
 
     public void Update()
     {
         //display the player health in UI
-                    //playerHealth.text = pL.ToString();
-        //Debug.Log(playerLife);
+        playerHealth.text = pL.ToString();
+        playerArmour.text = pA.ToString();
+
+        playerHealthBar.value = pL;
+        playerArmourBar.value = pA;
+
+        if (pL < 0)
+            pL = 0;
+
+        if (pL > 100)
+            pL = 100;
+
+        if (pA < 0)
+            pA = 0;
+
+        if (pA > 100)
+            pA = 100;
     }
 }
