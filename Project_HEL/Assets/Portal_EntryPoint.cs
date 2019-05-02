4f279337653c4f19f3333ba9fd3fe7979abd5b87
nikolasjklein <the@nikolasjklein.com>
2019-05-02 19:48:24 +0200

    Nikolas - added Portals (still broken af)

---
+++ Project_HEL/Assets/Portal_EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal_EntryPoint : MonoBehaviour
+{
+    public Transform PortalExitPoint;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.position = PortalExitPoint.transform.position;
+            other.transform.rotation = PortalExitPoint.transform.rotation;
+        }
+
+        Debug.Log (other.transform.position);
+        Debug.Log (PortalExitPoint.transform.position);
+    }
+}
